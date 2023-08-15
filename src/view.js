8ab47777312aac01ea134e561fab53982045a099
mohamed98 <mohamedatta447@gmail.com>
2023-08-15 07:16:47 +0300

    fetch data in console

--- src/view.js
+++ src/view.js
@@ -1,3 +1,17 @@
-/* eslint-disable no-console */
-console.log( 'Hello World! (from wpc-wptavern-block-slider block)' );
-/* eslint-enable no-console */
+
+async function fetchData() {
+    try {
+      const response = await fetch('https://wptavern.com/wp-json/wp/v2/posts/');
+      
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+  
+  fetchData();
