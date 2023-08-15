3049cb10b977e3951b7f14583a9e16fd8f5eae65
mohamed98 <mohamedatta447@gmail.com>
2023-08-15 07:36:35 +0300

    display data list in the front

--- src/render.php
+++ src/render.php
@@ -3,6 +3,7 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 ?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
+<p id="root" <?php echo get_block_wrapper_attributes(); ?>>
 	<?php esc_html_e( ' wptavern block slider – hello from a dynamic block!', 'wptavern-block-slider' ); ?>
+
 </p>
--- src/view.js
+++ src/view.js
@@ -15,3 +15,30 @@ async function fetchData() {
   }
   
   fetchData();
+
+  export function App() {
+    const [posts, setPosts] = React.useState([]);
+    // fetch data from API and display it as list
+    React.useEffect(() => {
+      fetch('https://wptavern.com/wp-json/wp/v2/posts/')
+        .then((response) => response.json())
+        .then((data) => setPosts(data));
+    }, []);
+
+    return (
+      <div>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>{post.title.rendered}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(
+      <App />,
+      document.querySelector('#root'),
+    )
+  })
